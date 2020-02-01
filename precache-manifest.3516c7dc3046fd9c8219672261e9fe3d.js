bd91776fd701c7ae303bd81a8579aa1bb8e4ecb3
AhmedMuhammedElsaid <ahmed.muhammed.elsaid@gmail.com>
2020-02-01 19:01:02 +0200

    Updates

---
+++ precache-manifest.3516c7dc3046fd9c8219672261e9fe3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17636edaa14e525c965dfc2aac72e6c5",
+    "url": "/React-Template/./index.html"
+  },
+  {
+    "revision": "0bbf7bb00038269672b8",
+    "url": "/React-Template/./static/css/main.720a81da.chunk.css"
+  },
+  {
+    "revision": "adddc9c306690cd3bc23",
+    "url": "/React-Template/./static/js/2.2d6710cc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React-Template/./static/js/2.2d6710cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bbf7bb00038269672b8",
+    "url": "/React-Template/./static/js/main.b39369d4.chunk.js"
+  },
+  {
+    "revision": "092f57f0d3f39cc75bab",
+    "url": "/React-Template/./static/js/runtime-main.6fb9b8fd.js"
+  }
+]);
